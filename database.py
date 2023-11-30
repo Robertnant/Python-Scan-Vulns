1e2caadacba0d74791614fd9ba8f6f4215c1578a
Robertnant <robertnantchouangn@gmail.com>
2023-11-30 23:48:42 +0100

    Implement function to compare scan results with known vulnerabilities

--- database.py
+++ database.py
@@ -9,106 +9,6 @@ from datetime import datetime, timedelta
 OUTDATED_CVE_TIMEDELTA = 7
 VULNERABILITIES_DATABASE_PATH = 'vulnerabilities_all_years.db'
 
-
-def fetch_vulnerabilities(cursor, service_name, version):
-    cursor.execute('''
-        SELECT cve_id, description, severity
-        FROM vulnerabilities
-        WHERE JSON_ARRAY_LENGTH(affected_software) > 0 AND (
-            EXISTS (
-                SELECT 1
-                FROM json_each(affected_software)
-                WHERE 
-                    json_extract(value, '$.vendor') = ? AND
-                    json_extract(value, '$.product') = ? AND
-                    json_extract(value, '$.version') = ?
-            )
-        )
-    ''', (service_name, 'junos', version))  # Example with 'junos' as the fixed product, modify as needed
-    return cursor.fetchall()
-
-def check_vulnerabilities():
-    # Read the file containing exposed devices' information
-    with open('all_exposed_devices.json') as file:
-        exposed_devices_data = json.load(file)
-
-    # Connect to the SQLite database
-    conn = sqlite3.connect(VULNERABILITIES_DATABASE_PATH)
-    cursor = conn.cursor()
-
-    # List to store vulnerabilities information
-    vulnerabilities_info = []
-
-    # Iterate through each exposed device
-    for exposed_device in exposed_devices_data.get('all_exposed_devices', []):
-        ip_address = exposed_device.get('ip_address', '')
-        
-        # Iterate through services of each exposed device
-        for service in exposed_device.get('services', []):
-            service_name = service.get('name', '')
-            service_version = service.get('version', '')
-
-            # Fetch vulnerabilities for the service name and version
-            vulnerabilities = fetch_vulnerabilities(cursor, service_name, service_version)
-
-            # Append vulnerabilities information to the list
-            for cve_id, description, severity in vulnerabilities:
-                vulnerabilities_info.append({
-                    'ip_address': ip_address,
-                    'cve_id': cve_id,
-                    'description': description,
-                    'severity': severity,
-                    'recommendations': 'Add your recommendation here if needed.',
-                })
-
-    # Close the connection to the database
-    conn.close()
-
-    print(vulnerabilities_info)
-    return vulnerabilities_info
-
-def retrieve_vulnerabilities(database_path, limit=10):
-    # Connect to the SQLite database
-    conn = sqlite3.connect(database_path)
-    cursor = conn.cursor()
-
-    # Fetch vulnerabilities from the database
-    cursor.execute(f'SELECT * FROM vulnerabilities LIMIT {limit}')
-    results = cursor.fetchall()
-
-    # Process and print retrieved vulnerabilities
-    retrieved_vulnerabilities = []
-    for result in results:
-        vulnerability = {
-            'cve_id': result[1],
-            'description': result[2],
-            'severity': result[3],
-            'affected_software': json.loads(result[4]),
-            'references_data': json.loads(result[5]),
-        }
-        retrieved_vulnerabilities.append(vulnerability)
-
-    # Close the connection
-    conn.close()
-
-    return retrieved_vulnerabilities
-
-def should_download_files(download_folder, start_year, end_year):
-    # Check if NVD CVE files for the given years are already present
-    for year in range(start_year, end_year + 1):
-        json_file_name = f'nvdcve-1.1-{year}.json'
-        json_file_path = os.path.join(download_folder, json_file_name)
-        if not os.path.isfile(json_file_path):
-            return True
-
-        # Check if the existing file is older than one week
-        one_week_ago = datetime.now() - timedelta(days=OUTDATED_CVE_TIMEDELTA)
-        file_modified_time = datetime.fromtimestamp(os.path.getmtime(json_file_path))
-        if file_modified_time < one_week_ago:
-            return True
-
-    return False
-
 def download_cve():
     # Create a folder to store the ZIP files
     download_folder = 'nvd_cve_zip_files'
@@ -123,7 +23,7 @@ def download_cve():
 
     # Download and extract NVD CVE ZIP files for each year
     if not should_download_files(download_folder, start_year, end_year):
-        print('Database already exists. The existing database will be used.')
+        print('Database already exists and is up to date. The existing database will be used.')
         return
     for year in range(start_year, end_year + 1):
         zip_file_name = f'nvdcve-1.1-{year}.json.zip'
@@ -229,9 +129,113 @@ def download_cve():
     print('Data from all years has been stored in the SQLite database.')
 
 
+def fetch_vulnerabilities(cursor, service_name, version):
+    cursor.execute('''
+        SELECT cve_id, description, severity
+        FROM vulnerabilities
+        WHERE JSON_ARRAY_LENGTH(affected_software) > 0 AND (
+            EXISTS (
+                SELECT 1
+                FROM json_each(affected_software)
+                WHERE 
+                    json_extract(value, '$.vendor') = ? AND
+                    json_extract(value, '$.product') = ? AND
+                    json_extract(value, '$.version') = ?
+            )
+        )
+    ''', (service_name, 'junos', version))  # Example with 'junos' as the fixed product, modify as needed
+    return cursor.fetchall()
+
+
+def check_vulnerabilities():
+    # Update CVE if needed.
+    download_cve()
+
+    # Read the file containing exposed devices' information
+    with open('all_exposed_devices.json') as file:
+        exposed_devices_data = json.load(file)
+
+    # Connect to the SQLite database
+    conn = sqlite3.connect(VULNERABILITIES_DATABASE_PATH)
+    cursor = conn.cursor()
+
+    # List to store vulnerabilities information
+    vulnerabilities_info = []
+
+    # Iterate through each exposed device
+    for exposed_device in exposed_devices_data.get('all_exposed_devices', []):
+        ip_address = exposed_device.get('ip_address', '')
+        
+        # Iterate through services of each exposed device
+        for service in exposed_device.get('services', []):
+            service_name = service.get('name', '')
+            service_version = service.get('version', '')
+
+            # Fetch vulnerabilities for the service name and version
+            vulnerabilities = fetch_vulnerabilities(cursor, service_name, service_version)
+
+            # Append vulnerabilities information to the list
+            for cve_id, description, severity in vulnerabilities:
+                vulnerabilities_info.append({
+                    'ip_address': ip_address,
+                    'cve_id': cve_id,
+                    'description': description,
+                    'severity': severity,
+                    'recommendations': 'Add your recommendation here if needed.',
+                })
+
+    # Close the connection to the database
+    conn.close()
+
+    print(vulnerabilities_info)
+    return vulnerabilities_info
+
+def retrieve_vulnerabilities(database_path, limit=10):
+    # Connect to the SQLite database
+    conn = sqlite3.connect(database_path)
+    cursor = conn.cursor()
+
+    # Fetch vulnerabilities from the database
+    cursor.execute(f'SELECT * FROM vulnerabilities LIMIT {limit}')
+    results = cursor.fetchall()
+
+    # Process and print retrieved vulnerabilities
+    retrieved_vulnerabilities = []
+    for result in results:
+        vulnerability = {
+            'cve_id': result[1],
+            'description': result[2],
+            'severity': result[3],
+            'affected_software': json.loads(result[4]),
+            'references_data': json.loads(result[5]),
+        }
+        retrieved_vulnerabilities.append(vulnerability)
+
+    # Close the connection
+    conn.close()
+
+    return retrieved_vulnerabilities
+
+def should_download_files(download_folder, start_year, end_year):
+    # Check if NVD CVE files for the given years are already present
+    for year in range(start_year, end_year + 1):
+        json_file_name = f'nvdcve-1.1-{year}.json'
+        json_file_path = os.path.join(download_folder, json_file_name)
+        if not os.path.isfile(json_file_path):
+            return True
+
+        # Check if the existing file is older than one week
+        one_week_ago = datetime.now() - timedelta(days=OUTDATED_CVE_TIMEDELTA)
+        file_modified_time = datetime.fromtimestamp(os.path.getmtime(json_file_path))
+        if file_modified_time < one_week_ago:
+            return True
+
+    return False
+
+
 # Example usage:
 # download_cve()
-retrieved_data = retrieve_vulnerabilities(VULNERABILITIES_DATABASE_PATH, limit=5)
+# retrieved_data = retrieve_vulnerabilities(VULNERABILITIES_DATABASE_PATH, limit=5)
 
 # # Print retrieved vulnerabilities
 # for index, vulnerability in enumerate(retrieved_data, start=1):
